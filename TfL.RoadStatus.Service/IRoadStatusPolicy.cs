f00bb2ec55291af896bac58c89304669f351622b
Savas Cilve <savascilve@lebilabs.com>
2019-04-28 17:29:07 +0300

    Implemented road status policy.

---
+++ TfL.RoadStatus.Service/IRoadStatusPolicy.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using TfL.RoadStatus.Service.Contract;
+
+namespace TfL.RoadStatus.Service
+{
+    public interface IRoadStatusPolicy
+    {
+        Task<IResponse> GetRoadStatus(GetRoadStatusRequest request);
+    }
+}
---
+++ TfL.RoadStatus.Service/RoadStatusPolicy.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using TfL.RoadStatus.Service.Contract;
+
+namespace TfL.RoadStatus.Service
+{
+    public class RoadStatusPolicy : IRoadStatusPolicy
+    {
+        // to make it simpler
+        private const string BaseUrl = "https://api.tfl.gov.uk/Road";
+
+        public async Task<IResponse> GetRoadStatus(GetRoadStatusRequest request)
+        {
+            if (request is null)
+                return new ExceptionResponse
+                {
+                    Result = new ExceptionDto
+                    {
+                        // can be enum
+                        ExceptionType = "ArgumentNullException",
+                        HttpStatus = "BadRequest",
+                        HttpStatusCode = 400,
+                        Message = "Request cannot be null.",
+                        RelativeUrl = "/Road/",
+                        TimeStampUtc = DateTime.UtcNow.ToLongDateString()
+                    }
+                };
+
+            if (string.IsNullOrEmpty(request.RoadName))
+                return new ExceptionResponse
+                {
+                    Result = new ExceptionDto
+                    {
+                        // can be enum
+                        ExceptionType = "ArgumentNullException",
+                        HttpStatus = "BadRequest",
+                        HttpStatusCode = 400,
+                        Message = "Road name cannot be null.",
+                        RelativeUrl = "/Road/",
+                        TimeStampUtc = DateTime.UtcNow.ToLongDateString()
+                    }
+                };
+
+            HttpClient httpClient = new HttpClient();
+
+            var proxyResponse = await httpClient.GetAsync($"{BaseUrl}/{request.RoadName}?app_id={request.AppId}&app_key={request.AppKey}");
+
+            if (proxyResponse.IsSuccessStatusCode)
+            {
+                var roadStatus = new List<RoadStatusDto>();
+                JsonConvert.PopulateObject(await proxyResponse.Content.ReadAsStringAsync(), roadStatus);
+
+                return new RoadStatusResponse { Result = roadStatus[0] };
+            }
+            else
+            {
+                var exception = new ExceptionDto();
+                JsonConvert.PopulateObject(await proxyResponse.Content.ReadAsStringAsync(), exception);
+
+                return new ExceptionResponse { Result = exception };
+            }
+        }
+    }
+}
