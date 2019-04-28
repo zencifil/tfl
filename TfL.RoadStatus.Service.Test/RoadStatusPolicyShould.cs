357364fef1ff68702622b060e0fde98a58e5c172
Savas Cilve <savascilve@lebilabs.com>
2019-04-28 18:01:38 +0300

    Added some unit tests.

---
+++ TfL.RoadStatus.Service.Test/RoadStatusPolicyShould.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using TfL.RoadStatus.Service.Contract;
+using FluentAssertions;
+
+namespace TfL.RoadStatus.Service.Test
+{
+    [TestFixture]
+    public class RoadStatusPolicyShould
+    {
+        private RoadStatusPolicy _policy;
+
+        [SetUp]
+        public void Setup()
+        {
+            _policy = new RoadStatusPolicy();
+        }
+
+        [Test]
+        public void ReturnExceptionResponse_WhenRequestIsNullAsync()
+        {
+            _policy.GetRoadStatus(default(GetRoadStatusRequest)).Should().BeEquivalentTo(new ExceptionResponse
+            {
+                Result = new ExceptionDto
+                {
+                    ExceptionType = "ArgumentNullException",
+                    HttpStatus = "BadRequest",
+                    HttpStatusCode = 400,
+                    Message = "Request cannot be null.",
+                    RelativeUrl = "/Road/",
+                    TimeStampUtc = DateTime.UtcNow.ToLongDateString()
+                }
+            });
+        }
+
+        [Test]
+        public void ReturnExceptionResponse_WhenRoadNameIsEmpty()
+        {
+            _policy.GetRoadStatus(new GetRoadStatusRequest { RoadName = string.Empty }).Should().BeEquivalentTo(new ExceptionResponse
+            {
+                Result = new ExceptionDto
+                {
+                    ExceptionType = "ArgumentNullException",
+                    HttpStatus = "BadRequest",
+                    HttpStatusCode = 400,
+                    Message = "Request cannot be null.",
+                    RelativeUrl = "/Road/",
+                    TimeStampUtc = DateTime.UtcNow.ToLongDateString()
+                }
+            });
+        }
+
+
+    }
+}
